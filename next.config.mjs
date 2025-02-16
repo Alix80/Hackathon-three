f9328dba34979424374983f5bf42966dfa58228b
saweraAli <148621903+Alix80@users.noreply.github.com>
2025-02-16 11:31:19 +0500

    Fixed punycode issue and updated dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['cdn.sanity.io']
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/**", // Agar specific path hai to use karo, warna /** rakho
+      },
+    ],
+  },
 };
 
 export default nextConfig;
